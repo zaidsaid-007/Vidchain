cd510ce86260cd0c9022303eb5420f9808504a8f
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-13 23:47:59 +0300

    new beginnings

--- src/Vidchain_frontend/vidchain/src/App.js
+++ src/Vidchain_frontend/vidchain/src/App.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router ,Routes , Route ,Link ,Outlet} from 'react-router-dom';
-import Home from './Home';
-function App(){
-  return 
-  <div className="app">
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />}>
+import React from 'react'
 
-        </Route>
-
-      </Routes>
-    </Router>
-
-
-
-  </div>
-
+function App() {
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
-
-export default App;
+export default App
