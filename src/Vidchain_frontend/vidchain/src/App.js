763a746dae20c6f581caaede15429c1fa32faefb
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-13 17:24:58 +0300

    react router dom

--- src/Vidchain_frontend/vidchain/src/App.js
+++ src/Vidchain_frontend/vidchain/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import './App.css';
-
+import {BrowserRouter as Router ,Routes , Route ,Link ,Outlet} from 'react-router-dom';
+import Home from './Home';
 function App(){
-  return <div className='App'>React App</div>;
+  return 
+  <div className="app">
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}>
+
+        </Route>
+
+      </Routes>
+    </Router>
+
+
+
+  </div>
+
 }
 
 
