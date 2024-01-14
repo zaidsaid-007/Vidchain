55aad47df3294b17d853e24baf98a601a21273e0
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-14 13:29:27 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import {Stack} from '@mui/material'
+import {link} from 'react-router-dom'
+
+import { logo } from './utils/constants'
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+  <Stack 
+  direction = "row" 
+  alignItems="center" 
+  p={2} 
+  sx={{position: 'sticky',background:'#021024',top:0, justifyContent:'space-between'}}>
+  </Stack>
 }
 
-export default Navbar
+export default Navbar;
