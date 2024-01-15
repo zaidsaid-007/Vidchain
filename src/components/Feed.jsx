cc6343ddaeafb784b888bb7463ad026165069778
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-15 13:00:56 +0300

    Sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+import { Sidebar } from '.'
 
 
 function Feed() {
@@ -8,7 +9,9 @@ function Feed() {
     <Stack sx={{ flexDirection : { sx:"column", md:"row" }}}
     >
       <Box sx={{height:{sx: 'auto', md:'92vh'}, borderRight: '1px solid #052659', px:{ sx:0,md: 2 }}} >
-        Sidebar
+        <Sidebar>
+
+        </Sidebar>
         <Typography className='coprright' variant='body2' sx ={{ mt :1.5, color:'#fff' }}>
            Copyright 2024 Vichain Media
         </Typography>
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+import { categories } from './utils/constants'
+
+
+const selectedCategory ='New';
+
+
+const Sidebar = () => {
+  return (
+    <Stack
+        direction = 'row'
+        sx= {{
+            overflowY:'auto',
+            height: {sx: 'auto', md: '95%'},
+            flexDirection:{ md: 'column'},
+            
+
+        }}
+    >
+        {categories.map((Category)=> (
+            <button
+                className='category-btn'
+                style={{
+                    backgroundColor : Category.name === selectedCategory && '#052659' ,
+                    color : 'white'
+                }}
+            >
+                <span style={{color : Category.name === selectedCategory ? 'white' : '#052659', marginRight: '15px' }} >{Category.icon}</span>
+                <span style={{opacity: Category.name === selectedCategory ? '1':'0.8' }} >{Category.name}</span>
+            </button>
+        ))}
+      
+    </Stack>
+  )
+}
+
+export default Sidebar
