82aee778765d2e0e74ecc18f0e716880ad077499
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-15 18:18:24 +0300

    Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -16,6 +16,11 @@ function Feed() {
            Copyright 2024 Vichain Media
         </Typography>
       </Box>
+      <Box p={2} sx= {{overflowY: 'auto', height: '90vh',flex:2 }} >
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{color: 'white'}} >
+          New <span style={{color : '#052659 '}} >Videos</span>
+        </Typography>
+      </Box>
       
     </Stack>
   )
