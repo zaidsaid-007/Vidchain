7b37c76ea0730e40dec558a49a9298639df228ea
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-15 10:50:13 +0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import {Box} from '@mui/material'
+import {Box} from '@mui/material';
 
 import{Feed,Navbar,VideoDetail,ChannelDetail,SearchFeed} from './components'
 
