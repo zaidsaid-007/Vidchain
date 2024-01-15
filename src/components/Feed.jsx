389c7d00ebcc2f9da88258bca1657144e1abc492
zaidsaid-007 <zaidsaid007@gmail.com>
2024-01-15 12:15:03 +0300

    feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+
 
 function Feed() {
   return (
-    <div>
+    <Stack sx={{ flexDirection : { sx:"column", md:"row" }}}
+    >
+      <Box sx={{height:{sx: 'auto', md:'92vh'}, borderRight: '1px solid #052659', px:{ sx:0,md: 2 }}} >
+        Sidebar
+        <Typography className='coprright' variant='body2' sx ={{ mt :1.5, color:'#fff' }}>
+           Copyright 2024 Vichain Media
+        </Typography>
+      </Box>
       
-    </div>
+    </Stack>
   )
 }
 
